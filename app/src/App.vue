77e6f1648a4914895ecd66d4ceedfaf7eeb0105c
Damn Peggy <ericrusso97@gmail.com>
2019-04-05 19:35:15 -0400

    shitty frontend

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,20 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SplashForm/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import SplashForm from './components/SplashForm.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+   SplashForm 
   }
 }
 </script>
+
 
 <style>
 #app {
---
+++ app/src/components/SplashForm.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="mainblock">
+    <div class="header">
+        <b-nav pills>
+          <b-nav-item v-on:click="activeTab=1" v-bind:active="activeTab === 1">Teacher</b-nav-item>
+          <b-nav-item v-on:click="activeTab=2" v-bind:active="activeTab === 2">Student</b-nav-item>
+        </b-nav>
+    </div>
+    <form v-if="activeTab === 1" id="createRoom">
+      <b-form-input id="roomName" v-model="text" placeholder="Classroom Name"></b-form-input>
+      <b-button id="submit">Create Classroom!</b-button>
+    </form>
+    <form v-else id="joinRoom">
+      <b-form-input id="roomID" v-model="text" placeholder="Classroom ID"></b-form-input>
+      <b-button id="submit">Join Room</b-button>
+    </form>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'SplashForm',
+  data: function()  {
+    return { activeTab : 1 };
+  },
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.mainblock {
+  /*text-align: center;*/
+  margin: 0 auto 0 auto;
+  width: 50%;
+}
+</style>
